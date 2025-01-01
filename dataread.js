bf58cd3ea8f003f5491d300d91057529d72c56b5
2boom <97739181+2boom-ua@users.noreply.github.com>
2025-01-01 14:17:45 +0200

    Add files via upload

---
+++ dataread.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2boom 2023  
+(async () => {
+	var sells = [];
+	let response = await fetch("four.json");
+	if (response.ok) {
+ 		let data = await response.json();
+		for(let i = 0; i < 36; i++) {
+			sells.push("4:" + i);
+		}
+		//console.log(data[0]);
+		for(let id_sell = 0; id_sell < sells.length; id_sell++) {
+			document.getElementById("name#" + sells[id_sell]).textContent = data[sells[id_sell]]["name"];
+			document.getElementById("icon#" + sells[id_sell]).src = data[sells[id_sell]]["icon"];
+			document.getElementById("link#" + sells[id_sell]).href = data[sells[id_sell]]["link"];
+		}
+    }
+})();
+
